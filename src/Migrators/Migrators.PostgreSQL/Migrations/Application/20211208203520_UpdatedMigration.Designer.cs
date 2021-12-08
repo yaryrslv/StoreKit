ee540ba314a2ac00ccd7b87ba7ae88af7d6a8ca0
Yaroslav <yaryrslv@gmail.com>
2021-12-08 22:36:59 +0200

    Update migration

--- src/Migrators/Migrators.PostgreSQL/Migrations/Application/20211208203520_UpdatedMigration.Designer.cs
+++ src/Migrators/Migrators.PostgreSQL/Migrations/Application/20211208203520_UpdatedMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StoreKit.Domain.Entities.Catalog;
@@ -12,9 +13,10 @@ using StoreKit.Infrastructure.Persistence;
 namespace Migrators.PostgreSQL.Migrations.Application
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211208203520_UpdatedMigration")]
+    partial class UpdatedMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
