a6a76c8f0f92bc6ebe563648f6835d6b435a7193
Yaroslav <yaryrslv@gmail.com>
2021-12-12 14:10:44 +0200

    Add Migration

---
+++ src/Migrators/Migrators.PostgreSQL/Migrations/Application/20211212121030_SuperMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.PostgreSQL.Migrations.Application
+{
+    public partial class SuperMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
